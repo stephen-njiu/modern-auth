6263e8411bd5f88c733a1376edc129b0391c59a3
njiutheguru <stephennjiu@gmail.com>
2025-11-25 04:44:09 +0300

    deploy v2

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
-import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = config.plugins ?? [];
-      config.plugins.push(new PrismaPlugin());
-    }
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
